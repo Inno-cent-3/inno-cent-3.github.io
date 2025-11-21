2bfcf877085dbde7e582b7938d66d430ed3b2eef
Innovic001 <innocent.amini@dcmail.ca>
2025-11-20 19:21:31 -0500

    Added comments to Lab 4 Part 2

--- LABS/lab4/part2/js/main.js
+++ LABS/lab4/part2/js/main.js
@@ -1,3 +1,10 @@
+/*
+Name: Innocent Amini
+	Date: 20 November 2025
+	File: main.js
+	Desc: This is the JavaScript file for the lab 4 assignment part 2.
+*/
+
 const displayedImage = document.querySelector(".displayed-img");
 const thumbBar = document.querySelector(".thumb-bar");
 
