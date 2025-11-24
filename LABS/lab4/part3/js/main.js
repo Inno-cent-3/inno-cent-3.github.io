e70bf35ba46d052020b2279001dfc8f86042b17a
Innovic001 <innocent.amini@dcmail.ca>
2025-11-23 22:10:57 -0500

    Updated the balls data

--- LABS/lab4/part3/js/main.js
+++ LABS/lab4/part3/js/main.js
@@ -22,7 +22,7 @@ function random(min, max) {
 // function to generate random color
 
 function randomRGB() {
-  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+    return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
 // Ball constructor
@@ -34,16 +34,40 @@ class Ball {
         this.velX = velX;
         this.velY = velY;
         this.color = color;
-        this.size = size;
-    }   
+        this.size = size;   
 }
 
+
 // draw method
-class Ball {
+
     draw() {
         ctx.beginPath();
         ctx.fillStyle = this.color;
         ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
         ctx.fill();
+    
+}
+
+// update method
+
+    update() {
+    if (this.x + this.size >= width) {
+      this.velX = -this.velX;
+    }
+
+    if (this.x - this.size <= 0) {
+      this.velX = -this.velX;
+    }
+
+    if (this.y + this.size >= height) {
+      this.velY = -this.velY;
+    }
+
+    if (this.y - this.size <= 0) {
+      this.velY = -this.velY;
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
     }
 }
