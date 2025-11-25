059ef2b737af0bee532956a5a063fea357cd6b99
Innovic001 <innocent.amini@dcmail.ca>
2025-11-25 15:40:28 -0500

    Update main.js
    Added the EvilCircle Definition and the constructor to enable the user to move the evilcircle around the screen.

--- LABS/lab4/part4/js/main.js
+++ LABS/lab4/part4/js/main.js
@@ -101,6 +101,32 @@ class Ball extends Shape {
   }
 }
 
+
+// EvilCircle subclass
+class EvilCircle extends Shape {
+  constructor(x, y) {
+    super(x, y, 20, 20);
+    this.color = "white";
+    this.size = 10;
+
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+    });
+  }
+
 // create array to store balls
 
 const balls = [];
